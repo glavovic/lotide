5b1205e63c151374d1c612e076e830bf9e97b09c
Armin Glavovic <glavovic.armin@gmail.com>
2022-01-17 22:58:42 +0000

    changed index.js object to es6 shorthand format

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const takeUntil = require('./takeUntil');
 const assertObjectsEqual = require('./assertObjectsEqual')
@@ -16,18 +16,18 @@ const without = require('./without')
 
 module.exports = {
 
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  takeUntil: takeUntil,
-  assertObjectsEqual: assertObjectsEqual,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  without: without
+  head,
+  tail,
+  middle,
+  takeUntil,
+  assertObjectsEqual,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  without
 
 };
